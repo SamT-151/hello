ca628739a7cece679fcf98807cd02139fcc74b77
SamT-151 <reachsamthompson@gmail.com>
2019-08-29 17:17:42 -0500

    Create hello.c
    Now also prints my major as per the August 29 hack assignment.

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Sam Thompson & Ashley Nguyen
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Made by Sam and Ashley\n");
+  printf("Sam is majoring in Physics!\n");
+
+  return 0;
+}
